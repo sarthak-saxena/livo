150c9e3cbad33f0af87ab142a4d3d2dcb8831f01
Sarthak saxena <>
2021-06-13 01:28:21 +0530

    add build-lib command

---
+++ index.ts
@@ -0,0 +1,7 @@
+import App from "./src/App";
+import { ConferenceMode } from "./src/types/App";
+import { Attendee, Room } from "./src/types/Conference";
+import { VoxeetConfig } from "./src/types/Voxeet";
+
+export { App, ConferenceMode };
+export type { Attendee, Room, VoxeetConfig };
--- src/index.ts
+++
@@ -1,7 +0,0 @@
-import App from "./App";
-import { ConferenceMode } from "./types/App";
-import { Attendee, Room } from "./types/Conference";
-import { VoxeetConfig } from "./types/Voxeet";
-
-export { App, ConferenceMode };
-export type { Attendee, Room, VoxeetConfig };
