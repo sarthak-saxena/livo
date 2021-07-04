e069ea7057011257fbb15ff7abe6c9edb05f56e6
Sarthak saxena <>
2021-07-04 18:34:37 +0530

    fix mute mike bug

--- src/components/ui/Attendee.tsx
+++ src/components/ui/Attendee.tsx
@@ -93,7 +93,7 @@ const useDataFromDataSync = (attendee: Participant) => {
       attendeeDataSync.speaker === undefined
         ? enableMakeSpeakerButtonDefault
         : !attendeeDataSync.speaker;
-    muteMikeDefault = attendeeDataSync.mute || muteMikeDefault;
+    muteMikeDefault = attendeeDataSync.mute === undefined ? muteMikeDefault : attendeeDataSync.mute
   }
   return { enableMakeSpeakerButtonDefault, muteMikeDefault };
 };
