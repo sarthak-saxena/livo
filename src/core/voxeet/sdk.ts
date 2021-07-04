5e6a9bfa174a0db06a384bfb6bd94523a802cb8d
Sarthak saxena <>
2021-07-04 19:36:19 +0530

    fix invalid token expired bug

--- src/core/voxeet/sdk.ts
+++ src/core/voxeet/sdk.ts
@@ -16,7 +16,6 @@ export const initializeVoxeet = async (
 ): Promise<Conference | undefined> => {
   VoxeetSdk.initialize(config.consumerKey, config.consumerSecret);
   try {
-    await purgeVoxeetSession()
     await VoxeetSdk.session.open({
       name: `${creator.name} ${creator.isConferenceCreator ? "(admin)" : ""}`,
       externalId: creator.id,
