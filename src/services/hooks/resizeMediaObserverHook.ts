3e2bd78513f7b4dba66a42c26de071acfa8ccff3
Sarthak saxena <>
2021-07-04 14:06:06 +0530

    adjust small screensize as per height

--- src/services/hooks/resizeMediaObserverHook.ts
+++ src/services/hooks/resizeMediaObserverHook.ts
@@ -13,11 +13,12 @@ const initResizeObserver = () => {
     const element = document.getElementsByClassName(LivoAppContainer)[0];
     const resizeObserver = new ResizeObserver((entries) => {
       if (entries[0] && entries[0].contentBoxSize[0]) {
+        const height = entries[0].contentBoxSize[0].blockSize;
         const width = entries[0].contentBoxSize[0].inlineSize;
-        if (width < 500 && !lastInlineSizeSmall) {
+        if (width < 500 && height < 300 && !lastInlineSizeSmall) {
           callbackEventListener.call(ResizeMediaCallback, width);
           lastInlineSizeSmall = true;
-        } else if (width > 500 && lastInlineSizeSmall) {
+        } else if (width > 500 && height > 300 && lastInlineSizeSmall) {
           callbackEventListener.call(ResizeMediaCallback, width);
           lastInlineSizeSmall = false;
         }
